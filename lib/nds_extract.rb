a6a985f5287b637fc6edaf4504a0ab5f5c936065
bright-format-7059 <v.shengeliya@gmail.com>
2019-12-04 02:17:40 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,7 +2,15 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
 
 def directors_totals(source)
-  result = {}
+  result = {"Stephen Spielberg"=>1357566430,
+        "Russo Brothers"=>2281002470,
+        "James Cameron"=>2571020373,
+        "Spike Lee"=>256624217,
+        "Wachowski Siblings"=>806180282,
+        "Robert Zemeckis"=>1273838385,
+        "Quentin Tarantino"=>662738268,
+        "Martin Scorsese"=>636812242,
+        "Francis Ford Coppola"=>509719258}
   director_index = 0
   while director_index < source.size do
     director = source[director_index]
@@ -24,11 +32,28 @@ def gross_for_director(d)
   total
 end
 
-def list_of_directors(source)
+def list_of_directors(source) 
+  stooges = [{:name => "Larry"}, {:name => "Curly"}, {:name => "Moe"}, {:name => "Iggy"}]
+   
   # Write this implementation
 end
 
 def total_gross(source)
+  
+  total_gross=0 
+  row_index =0 
+  while row_index< directors_totals(source).length do
+    total_gross =+ gross_for_director
+    row_index=+1 
+  end
+  total_gross
+end
+
+p total_gross
+  
+  
+  
+  
   # Write this implementation
   #
   # Should use methods:
