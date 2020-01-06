9dceb6b767c9c3b88e1e99a580598a91348c2426
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-06 02:31:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,15 +2,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
 
 def directors_totals(source)
-  result = {"Stephen Spielberg"=>1357566430,
-        "Russo Brothers"=>2281002470,
-        "James Cameron"=>2571020373,
-        "Spike Lee"=>256624217,
-        "Wachowski Siblings"=>806180282,
-        "Robert Zemeckis"=>1273838385,
-        "Quentin Tarantino"=>662738268,
-        "Martin Scorsese"=>636812242,
-        "Francis Ford Coppola"=>509719258}
+  result = {}
   director_index = 0
   while director_index < source.size do
     director = source[director_index]
@@ -32,37 +24,82 @@ def gross_for_director(d)
   total
 end
 
-def list_of_directors(source) 
-  stooges = [{:name => "Larry"}, {:name => "Curly"}, {:name => "Moe"}, {:name => "Iggy"}]
-   
-  # Write this implementation
+def list_of_directors(source)
+  names = []
+  i = 0
+
+  while i < source.length do
+    names << source[i][:name]
+    i += 1
+  end
+
+  names
 end
 
 def total_gross(source)
-  
-  total_gross=0 
-  row_index =0 
-  while row_index< directors_totals(source).length do
-    total_gross =+ gross_for_director
-    row_index=+1 
+  dir_to_earnings_hash = directors_totals(source)
+  dir_names = list_of_directors(source)
+  i = 0
+
+  total = 0
+
+  while i < dir_names.length do
+    dir_name = dir_names[i]
+    total += dir_to_earnings_hash[dir_name]
+    i += 1
   end
-  total_gross
+
+  total
+end$LOAD_PATH.unshift(File.dirname(__FILE__))
+require_relative './directors_database'
+
+def directors_totals(source)
+  result = {}
+  director_index = 0
+  while director_index < source.size do
+    director = source[director_index]
+    result[director[:name]] = gross_for_director(director)
+    director_index += 1
+  end
+  result
 end
 
-p total_gross
-  
-  
-  
-  
-  # Write this implementation
-  #
-  # Should use methods:
-  # 1. directors_totals: returns a Hash of { dir_name => gross }
-  # 2. list_of_directors: names provides an Array of directors names (use
-  #
-  # Visit each key (i.e. director name), look up the value in the hash
-  # returned by directors_totals, and add it to a running total. When done,
-  # return the total
+def gross_for_director(d)
+  total = 0
+  index = 0
+
+  while index < d[:movies].length do
+    total += d[:movies][index][:worldwide_gross]
+    index += 1
+  end
+
+  total
 end
 
+def list_of_directors(source)
+  names = []
+  i = 0
 
+  while i < source.length do
+    names << source[i][:name]
+    i += 1
+  end
+
+  names
+end
+
+def total_gross(source)
+  dir_to_earnings_hash = directors_totals(source)
+  dir_names = list_of_directors(source)
+  i = 0
+
+  total = 0
+
+  while i < dir_names.length do
+    dir_name = dir_names[i]
+    total += dir_to_earnings_hash[dir_name]
+    i += 1
+  end
+
+  total
+end
